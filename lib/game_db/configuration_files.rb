4c27a10e04203837812db9485cd8f4205bdc0c22
Pabgon0 <pabgon0@gmail.com>
2019-09-12 02:07:03 -0500

    Setup the required gems for project files

---
+++ lib/game_db/configuration_files.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/game_db/cli'
+require_relative '../lib/game_db/gamedb'
+require_relative '../lib/game_db/scraper'
+require_relative '../lib/game_db/version'
