796a94a3034fffbd327ad5b125fa4968ef1706c3
Pabgon0 <pabgon0@gmail.com>
2019-09-12 21:37:57 -0500

    Setup the website scraper

--- lib/game_db/scraper.rb
+++ lib/game_db/scraper.rb
@@ -1,8 +1,13 @@
 class GameDb::Scraper
 
-    def website
-        Nokogiri::HTML(open("https://www.gamesdatabase.org/systems"))
+    def self.website
+        doc = Nokogiri::HTML(open("https://www.gamesdatabase.org/systems"))
+        doc.css("table.NoWrap tr")[1..-1].each do |row|
+            console = row.css("a")[1].text
+            maker = row.css("a")[2].text
+            type = row.css("td")[-2].text
+            game_amount = row.css("td")[-1].text
+            GameDb::Game.new(console, maker, type, game_amount)
+        end
     end
-
-    
 end
