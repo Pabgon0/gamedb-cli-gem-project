66587cf18e5c58785debd75323cd2538ace2da5f
Pabgon0 <pabgon0@gmail.com>
2019-09-12 21:24:51 -0500

    Initialize attributes and rename the class

--- lib/game_db/game.rb
+++ lib/game_db/game.rb
@@ -1,3 +1,18 @@
-class GameDb::Gamedb
+class GameDb::Game
+
+    attr_accessor :console, :maker, :type, :game_amount
+    @@all = []
+
+    def initialize(console=nil, maker=nil, type=nil, game_amount=nil)
+        @console = console
+        @maker = maker
+        @type = type
+        @game_amount = game_amount
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
